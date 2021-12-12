4ce4fc6c719f8ef62418e73416c8b86289a8a86e
VineethRRao <vineethr.cs19@bmsce.ac.in>
2021-12-12 16:03:23 +0530

    Home screen and splash screen

---
+++ lib/d.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:bestream/main.dart';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+@override
+_MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+@override
+void initState() {
+	super.initState();
+	Timer(Duration(seconds: 3),
+		()=>Navigator.pushReplacement(context,
+										MaterialPageRoute(builder:
+														(context) =>
+														Home()
+														)
+									)
+		);
+}
+@override
+Widget build(BuildContext context) {
+	return Container(
+	color: Color(0xff290B37),
+	child: Container(child: Image.asset('assets/images/splash.png' ),)
+	);
+}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+import 'd.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Home(),
+
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home:MyHomePage(),
+	debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -56,98 +64,533 @@ class Home extends StatelessWidget {
         ),  
         body:  SingleChildScrollView ( // this will make your body scrollable
   child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
       
    /// your parameters
-  
+    
           
         
           children: <Widget>[
 
-            
-            Container(
-              height:50,
-              width:100,
+         
+          Container(
+              height:20,
+              width:400,
               
             ),
- 
+          
+         
+
+          Stack( children: <Widget>[
+
+         
+            
+  
           Container(
-          height: 300.0,
-          width: 336.0,
-          color: Colors.black,
-          child: Container(
-            color: Colors.white,
-           child: new Container(child: Image.asset('assets/images/logo.jpeg' ),  
+                color: Colors.white,
+                child: Container(child: Image.asset('assets/images/post-4.jpg' )   
             ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'Crazy_Jack',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 2mins ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '334',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '31',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
           ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
+          ),
+          ],
+          ),
+          
+          Container(
+              height:10,
+              width:400,
+              
+            ),
+        
+           Stack( children: <Widget>[
+
+         
+            
+  
+          Container(
+                color: Colors.white,
+                child: Container(child: Image.asset('assets/images/post-2.jpg' )   
+            ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'The_Real_Sadhu',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 5mins ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '1k',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '561',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
+          ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
+          ),
+          ],
           ),
 
-    
-            Container(
+          Container(
               height:10,
-              width:100,
+              width:400,
               
             ),
- 
-            Container(
-          height: 300.0,
-          width: 336.0,
-          color: Colors.black,
-          child: Container(
+
+
+           Stack( children: <Widget>[
+
+         
             
-           
+  
+          Container(
                 color: Colors.white,
-               
-            child: new Container(child: Image.asset('assets/images/logo.jpeg' ),   
+                child: Container(child: Image.asset('assets/images/post-3.jpg' )   
             ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'Ni_killz',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 1hr ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '204',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '9',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
           ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
           ),
+          ],
+          ),
+
+           Container(
+              height:10,
+              width:400,
+              
+            ),
+
+
+           Stack( children: <Widget>[
+
+         
+            
+  
+          Container(
+                color: Colors.white,
+                child: Container(child: Image.asset('assets/images/post-1.jpg' )   
+            ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'Travel69freak',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 2days ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '31L',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '600',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
+          ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
+          ),
+          ],
+          ),
+
 Container(
               height:10,
-              width:100,
+              width:400,
               
             ),
- 
-           Container(
-          height: 300.0,
-          width: 336.0,
-          color: Colors.black,
-          child: Container(
+
+            Stack( children: <Widget>[
+
+         
             
-           
-                color: Colors.white,
-               
-            child: new Container(child: Image.asset('assets/images/logo.jpeg' ),
-            ),
-          ),
-          ),
-	Container(
-              height:10,
-              width:100,
-              
-            ),
- 
-	
-               Container(
-          height: 300.0,
-          width: 336.0,
-          color: Colors.black,
-          child: Container(
-            
-           
-                color: Colors.white,
-               
-            child: new Container(child: Image.asset('assets/images/logo.jpeg' ), 
-            ),
-          ),
-          ),
-	 
-              
+  
           Container(
-              height:50,
-              width:100,
+                color: Colors.white,
+                child: Container(child: Image.asset('assets/images/post-4.jpg' )   
+            ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'Crazy_Jack',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 2mins ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '334',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '31',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
+          ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
+          ),
+          ],
+          ),
+
+          Container(
+              height:10,
+              width:400,
               
             ),
- 
+
+
+           Stack( children: <Widget>[
+
+         
+            
+  
+          Container(
+                color: Colors.white,
+                child: Container(child: Image.asset('assets/images/post-3.jpg' )   
+            ),
+          ),  
+          
+         
+           Positioned(
+             
+              top: 10,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'Crazy_Jack',
+                  style: TextStyle(fontSize: 18,  color: Colors.black),
+                ),
+              ),),
+        
+              Positioned(
+             
+              top: 30,
+              left: 60,
+
+              child: Container(
+              child: Text(
+                  'posted 2mins ago',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+
+
+              Positioned(
+             
+              bottom: 13,
+              left: 40,
+
+              child: Container(
+              child: Text(
+                  '334',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+
+              
+
+              Positioned(
+             
+              bottom: 13,
+              left: 110,
+
+              child: Container(
+              child: Text(
+                  '31',
+                  style: TextStyle(fontSize: 16,  color: Colors.grey),
+                ),
+              ),),
+          
+          Positioned(
+          bottom: 10, left: 80,
+          child: Icon(Icons.comment_bank,) ,
+          ),
+             
+          Positioned(
+          bottom: 10, left: 15,
+         child: Icon(Icons.favorite) ,
+          ),
+          ],
+          ),
+
+             Container(
+              height:10,
+              width:400,
+              
+            ),
+
+        Container(
+          height: 50,
+          width:50,
+
+        child: Image.network('https://bison.usgs.gov/images/spinner2.gif')
+          
+          
+          ),
+
+              Container(
+              height:10,
+              width:400,
+              
+            ),
+
    
         ],
       // your widgets,
@@ -160,7 +603,7 @@ Container(
          bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Color(0xff290B37),
-        selectedItemColor: Colors.blue[700],
+        selectedItemColor:  Colors.grey[700],
         unselectedItemColor: Color(0xffffffff),
         selectedFontSize: 13,
         unselectedFontSize: 13,
@@ -174,19 +617,19 @@ Container(
           ),
           BottomNavigationBarItem(
             title: new Container(height: 0.0),
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.search_rounded),
           ),
            BottomNavigationBarItem(
          title: new Container(height: 0.0),
-            icon: Icon(Icons.add_a_photo),
+            icon: Icon(Icons.add_circle),
           ),
               BottomNavigationBarItem(
            title: new Container(height: 0.0),
-            icon: Icon(Icons.stream),
+            icon: Icon(Icons.tv_rounded),
           ),
           BottomNavigationBarItem(
             title: new Container(height: 0.0),
-            icon: Icon(Icons.shop_2),
+            icon: Icon(Icons.shop),
           ),
       
         ],
