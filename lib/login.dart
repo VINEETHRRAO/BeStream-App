adbf828e5ce3b1a2d6899562cef26ad906d95135
Vishal Khot <vishalkhot01@gmail.com>
2022-01-08 19:25:52 +0530

    Signup working

--- lib/login.dart
+++ lib/login.dart
@@ -8,13 +8,6 @@ import 'package:flutter_login/theme.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
-const users = {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-  'vineeth.yellur@gmail.com': 'vineeth',
-  '12345@gmail.com': '12345'
-};
-
 class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -33,30 +26,63 @@ class _LoginScreenState extends State<LoginScreen> {
       var jsonBody = json.encode(body);
       Response response = await post(uri, headers: {"Content-Type": "application/json"}, body: jsonBody);
       var responseMap = json.decode(response.body) as Map;
+      debugPrint(response.body);
 
        
 
     return Future.delayed(loginTime).then((_) {
       if (!responseMap['isAllowed']) {
-        return 'User not exists or Password does not match';
+        return 'User does not exist or Password does not match';
       }
       return null;
     });
   }
 
-  Future<String?> _signupUser(data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
+  Future<String?> _signupUser(SignupData data) async{
+    debugPrint('Email: ${data.name}, Password: ${data.password}, Username: ${data.additionalSignupData!['Username']}');
+    Map body = {
+      'email': data.name,
+      'username': data.additionalSignupData!['Username'],
+    };
+    var uri = Uri.parse("https://bestream-backend.herokuapp.com/users/check/duplicate");
+    var jsonBody = json.encode(body);
+    Response response = await post(uri, headers: {"Content-Type": "application/json"}, body: jsonBody);
+    debugPrint(response.body);
+    var responseMap = json.decode(response.body) as Map;
+
+    return Future.delayed(loginTime).then((_) async{
+      if(responseMap['done'] == 0){
+          if(responseMap['email'] == 1) {
+            return "Email already exists";
+          }
+          else {
+            return "Username already exists";
+          }
+      }
+      var uri = Uri.parse("https://bestream-backend.herokuapp.com/users/signup");
+      Map body = {
+        'email': data.name,
+        'username': data.additionalSignupData!['Username'],
+        'password': data.password,
+        'firstName': data.additionalSignupData!['First Name'],
+        'lastName': data.additionalSignupData!['Last Name'],
+      };
+      var jsonBody = json.encode(body);
+      Response response = await post(uri, headers: {"Content-Type": "application/json"}, body: jsonBody);
+      debugPrint(response.body);
+      responseMap = json.decode(response.body) as Map;
+      return Future.delayed(loginTime).then((_) async {
+          if(responseMap['done'] == 0) {
+            return "Connection error";
+          }
+          return null;
+      });
     });
   }
 
   Future<String> _recoverPassword(String name) {
     debugPrint('Name: $name');
     return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
       return '';
     });
   }
@@ -69,6 +95,7 @@ class _LoginScreenState extends State<LoginScreen> {
         'assets/images/transparent.png',
       ),
       onLogin: _authUser,
+      additionalSignupFields:[UserFormField(keyName: "Username"), UserFormField(keyName: "First Name"), UserFormField(keyName: "Last Name"),],
       onSignup: _signupUser,
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
